89d03307bd49f8ad04c1fb590481ea91c8fa167c
Dmitry Diachenko <dmytro.diachenko@spsoft.com>
2022-06-17 11:36:57 +0100

    234

--- .github/scripts/generate-markdown.sh
+++ .github/scripts/generate-markdown.sh
@@ -1,10 +1,12 @@
 echo "Script start"
-cd ../..
+cd ~.flutter_driver_test_sample
 ls
 echo "\n\n navigate to screenshoots \n\n"
 cd test/failures
 ls
-
+echo "start writing to file:::"
+touch files.txt
+ 
 for entry in  *.png ; do
-   echo "![$entry]( $(realpath $entry) )"
+   echo "![$entry]( $(realpath $entry) )" >> files.txt
 done
