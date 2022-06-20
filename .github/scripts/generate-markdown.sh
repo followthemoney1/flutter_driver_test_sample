8a43dc92cc8f705c847e8ad3884c27168dadaa10
Dmitry Diachenko <dmytro.diachenko@spsoft.com>
2022-06-20 12:22:54 +0100

    goldens

--- .github/scripts/generate-markdown.sh
+++ .github/scripts/generate-markdown.sh
@@ -9,7 +9,7 @@ touch files.txt
  
 for entry in  *.png ; do
    #https://stackoverflow.com/a/50428634
-   echo "![$entry]( https://firebasestorage.googleapis.com/v0/b/undeground-ji2/o/failures%2F$entry?alt=media )" 
+   echo "![$entry]( https://firebasestorage.googleapis.com/v0/b/undeground-ji2/o/failures%2F$entry?alt=media )" >> files.txt
    #or
    # echo `<img width="300" alt="$entry" src="https://firebasestorage.googleapis.com/v0/b/undeground-ji2/o/failures%2F$entry?alt=media">` >> files.txt
 done
